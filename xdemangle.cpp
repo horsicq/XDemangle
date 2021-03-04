bb8062f33b63fe3593db991c1b3377178f90a1bb
horsicq <horsicq@gmail.com>
2021-03-04 08:27:32 +0100

    Fix

--- xdemangle.cpp
+++ xdemangle.cpp
@@ -25,51 +25,312 @@ XDemangle::XDemangle(QObject *pParent) : QObject(pParent)
 
 }
 
-QString XDemangle::typeIdToString(XDemangle::TYPE type)
+QString XDemangle::modeIdToString(XDemangle::MODE mode)
 {
+    QString sResult="Unknown"; // mb TODO translate
+
+    switch(mode)
+    {
+        case MODE_UNKNOWN:      sResult=QString("Unknown");             break; // mb TODO translate
+        case MODE_MSVC:         sResult=QString("MSVC C++");            break;
+        case MODE_GCC_2XX:      sResult=QString("GCC 2.XX");            break;
+        case MODE_GCC_3XX:      sResult=QString("GCC 3.XX-X.XX");       break;
+        case MODE_WATCOM:       sResult=QString("Watcom");              break;
+        case MODE_BORLAND:      sResult=QString("Borland");             break;
+    }
+
+    return sResult;
+}
+
+QString XDemangle::typeIdToString(XDemangle::TYPE type, XDemangle::MODE mode)
+{
+    Q_UNUSED(mode) // TODO
+
     QString sResult="Unknown"; // mb TODO translate
 
     switch(type)
     {
         case TYPE_UNKNOWN:      sResult=QString("Unknown");             break; // mb TODO translate
-        case TYPE_MSVC:         sResult=QString("MSVC C++");            break;
-        case TYPE_GCC_2XX:      sResult=QString("GCC 2.XX");            break;
-        case TYPE_GCC_3XX:      sResult=QString("GCC 3.XX-X.XX");       break;
+        case TYPE_INT:          sResult=QString("int");                 break;
     }
 
     return sResult;
 }
 
-QString XDemangle::convert(QString sString,TYPE type)
+QString XDemangle::convert(QString sString, MODE mode)
 {
     QString sResult;
 
-    if(type==TYPE_MSVC)
+    if(sString!="")
     {
-        sResult=convert_MSVC(sString);
-    }
+        SYMBOL symbol={};
 
-    // TODO
-
-    return sResult;
-}
-
-QString XDemangle::convert_MSVC(QString sString)
-{
-    // ??$qbswap@$01@@YAPEAXPEBX_JPEAX@Z
-    // ?qHash@@YAIABVQGeoCoordinate@@I@Z
-    QString sResult;
-    // All C++ begins with ?
-
-    if(XBinary::isRegExpPresent("^\\?",sString)) // ? Customer type
-    {
-        sString=sString.mid(1,-1);
-        // TODO Check digits
-        if(XBinary::isRegExpPresent("^\\?\\$",sString)) // ?$
+        if(mode==MODE_MSVC)
         {
-            sResult="Test";
+            symbol=handle_MSVC(sString);
+        }
+
+        sResult=symbolToString(symbol);
+    }
+
+    return sResult;
+}
+
+QString XDemangle::symbolToString(XDemangle::SYMBOL symbol)
+{
+    QString sResult;
+
+    if(symbol.bValid)
+    {
+        if(symbol.objectClass==OC_STATICCLASSMEMBER)
+        {
+            sResult+=QString("static ");
+        }
+
+        if(symbol.storageClass==SC_CONST)
+        {
+            sResult+=QString("const ");
+        }
+        else if(symbol.storageClass==SC_VOLATILE)
+        {
+            sResult+=QString("volatile ");
+        }
+
+        sResult+=QString("%1 ").arg(typeIdToString(symbol.paramReturn.type,symbol.mode));
+
+        int nNumberOfNames=symbol.listNames.count();
+
+        for(int i=nNumberOfNames-1;i>=0;i--)
+        {
+            sResult+=symbol.listNames.at(i);
+
+            if(i)
+            {
+                sResult+="::";
+            }
         }
     }
 
     return sResult;
+}
+
+XDemangle::STRING XDemangle::readString(QString sString, XDemangle::MODE mode)
+{
+    STRING result={};
+
+    if(mode==MODE_MSVC)
+    {
+        result.sString=sString.section("@",0,0);
+
+        result.nSize=result.sString.size();
+
+        if(result.nSize)
+        {
+            if(result.sString!=sString)
+            {
+                result.nSize++;
+            }
+        }
+    }
+
+    return result;
+}
+
+XDemangle::SYMBOL XDemangle::handle_MSVC(QString sString)
+{
+    // ??$qbswap@$01@@YAPEAXPEBX_JPEAX@Z
+    // ?qHash@@YAIABVQGeoCoordinate@@I@Z
+    // int alpha / ?alpha@@3HA
+    SYMBOL result={};
+    // All C++ begins with ?
+
+    if(_compare(sString,"?"))
+    {
+        result.mode=MODE_MSVC;
+
+        QMap<QString,qint32> mapObjectClasses=getObjectClasses(MODE_MSVC);
+        QMap<QString,qint32> mapTypes=getTypes(MODE_MSVC);
+        QMap<QString,qint32> mapStorageClasses=getStorageClasses(MODE_MSVC);
+
+        sString=sString.mid(1,-1);
+
+        // Name
+        while(sString!="")
+        {
+            STRING _string=readString(sString,MODE_MSVC);
+
+            if(_string.nSize)
+            {
+                result.listNames.append(_string.sString);
+            }
+            else
+            {
+                break;
+            }
+
+            sString=sString.mid(_string.nSize,-1);
+        }
+
+        if(isSignaturePresent(sString,&mapObjectClasses))
+        {
+            SIGNATURE signature=getSignature(sString,&mapObjectClasses);
+            result.objectClass=(OC)signature.nValue;
+            sString=sString.mid(signature.nSize,-1);
+        }
+
+        if(isSignaturePresent(sString,&mapTypes))
+        {
+            SIGNATURE signature=getSignature(sString,&mapTypes);
+            result.paramReturn.type=(TYPE)signature.nValue;
+            sString=sString.mid(signature.nSize,-1);
+        }
+
+        while(sString!="") // TODO Check
+        {
+            SIGNATURE signature=getSignature(sString,&mapTypes);
+
+            if(signature.nSize)
+            {
+                PARAMETER parameter={};
+                parameter.type=(TYPE)signature.nValue;
+
+                result.listFunctionArguments.append(parameter);
+            }
+            else
+            {
+                break;
+            }
+
+            sString=sString.mid(signature.nSize,-1);
+        }
+
+        if(isSignaturePresent(sString,&mapStorageClasses))
+        {
+            SIGNATURE signature=getSignature(sString,&mapStorageClasses);
+            result.storageClass=(SC)signature.nValue;
+            sString=sString.mid(signature.nSize,-1);
+        }
+
+        if(sString=="") // TODO more checks
+        {
+            result.bValid=true;
+        }
+    }
+
+    return result;
+}
+
+bool XDemangle::_compare(QString sString, QString sSignature)
+{
+    bool bResult=false;
+
+    qint32 nSignatureSize=sSignature.size();
+
+    if(sString.size()>=nSignatureSize)
+    {
+        QString _sString=sString.left(nSignatureSize);
+
+        bResult=(_sString==sSignature);
+    }
+
+    return bResult;
+}
+
+bool XDemangle::isSignaturePresent(QString sString, QMap<QString, qint32> *pMap)
+{
+    bool bResult=false;
+
+    QMapIterator<QString,qint32> i(*pMap);
+
+    while(i.hasNext())
+    {
+        i.next();
+
+        if(_compare(sString,i.key()))
+        {
+            bResult=true;
+
+            break;
+        }
+    }
+
+    if(!bResult)
+    {
+    #ifdef QT_DEBUG
+        qDebug(sString.toLatin1().data());
+    #endif
+    }
+
+    return bResult;
+}
+
+XDemangle::SIGNATURE XDemangle::getSignature(QString sString, QMap<QString, qint32> *pMap)
+{
+    SIGNATURE result={};
+
+    QMapIterator<QString,qint32> i(*pMap);
+
+    while(i.hasNext())
+    {
+        i.next();
+
+        QString sKey=i.key();
+        qint32 nValue=i.value();
+
+        if(_compare(sString,sKey))
+        {
+            result.nSize=sKey.size();
+            result.nValue=nValue;
+
+            break;
+        }
+    }
+
+    if(result.nSize==0)
+    {
+    #ifdef QT_DEBUG
+        qDebug(sString.toLatin1().data());
+    #endif
+    }
+
+    return result;
+}
+
+QMap<QString, qint32> XDemangle::getObjectClasses(XDemangle::MODE mode)
+{
+    QMap<QString,qint32> mapResult;
+
+    if(mode==MODE_MSVC)
+    {
+        mapResult.insert("@2",OC_STATICCLASSMEMBER);
+        mapResult.insert("@3",OC_GLOBALOBJECT);
+    }
+
+    return mapResult;
+}
+
+QMap<QString, qint32> XDemangle::getTypes(XDemangle::MODE mode)
+{
+    QMap<QString,qint32> mapResult;
+
+    if(mode==MODE_MSVC)
+    {
+        mapResult.insert("H",TYPE_INT);
+    }
+
+    return mapResult;
+}
+
+QMap<QString, qint32> XDemangle::getStorageClasses(XDemangle::MODE mode)
+{
+    QMap<QString,qint32> mapResult;
+
+    if(mode==MODE_MSVC)
+    {
+        mapResult.insert("A",SC_NEAR);
+        mapResult.insert("B",SC_CONST);
+        mapResult.insert("C",SC_VOLATILE);
+    }
+
+    return mapResult;
 }
--- xdemangle.h
+++ xdemangle.h
@@ -22,26 +22,90 @@
 #define XDEMANGLE_H
 
 #include <QObject>
-#include "xbinary.h" // TODO remove
+#include <QMap>
 
 class XDemangle : public QObject
 {
     Q_OBJECT
 
 public:
-    enum TYPE
+    enum MODE
     {
-        TYPE_UNKNOWN=0,
-        TYPE_MSVC,
-        TYPE_GCC_2XX,
-        TYPE_GCC_3XX
+        MODE_UNKNOWN=0,
+        MODE_MSVC,
+        MODE_GCC_2XX,
+        MODE_GCC_3XX,
+        MODE_BORLAND,
+        MODE_WATCOM
         // TODO more
     };
 
+    enum TYPE
+    {
+        TYPE_UNKNOWN=0,
+        TYPE_VOID,
+        TYPE_INT
+    };
+
+    struct PARAMETER
+    {
+        TYPE type;
+    };
+
+    enum OC
+    {
+        OC_UNKNOWN=0,
+        OC_GLOBALOBJECT,
+        OC_STATICCLASSMEMBER
+    };
+
+    enum SC
+    {
+        SC_UNKNOWN=0,
+        SC_NEAR,
+        SC_CONST,
+        SC_VOLATILE
+    };
+
+    struct SYMBOL
+    {
+        bool bValid;
+        MODE mode;
+        QList<QString> listNames;
+        PARAMETER paramReturn;
+        QList<PARAMETER> listFunctionArguments;
+        OC objectClass;
+        SC storageClass;
+    };
+
     explicit XDemangle(QObject *pParent=nullptr);
-    static QString typeIdToString(TYPE type);
-    static QString convert(QString sString,TYPE type);
-    static QString convert_MSVC(QString sString);
+    QString modeIdToString(MODE mode);
+    QString typeIdToString(TYPE type,MODE mode);
+    QString convert(QString sString,MODE mode);
+
+private:
+    struct STRING
+    {
+        qint32 nSize;
+        QString sString;
+    };
+
+    struct SIGNATURE
+    {
+        qint32 nSize;
+        qint32 nValue;
+    };
+
+    QString symbolToString(SYMBOL symbol);
+    STRING readString(QString sString,MODE mode);
+    SYMBOL handle_MSVC(QString sString);
+    bool _compare(QString sString,QString sSignature);
+
+    bool isSignaturePresent(QString sString,QMap<QString,qint32> *pMap);
+    SIGNATURE getSignature(QString sString,QMap<QString,qint32> *pMap);
+    QMap<QString,qint32> getObjectClasses(MODE mode);
+    QMap<QString,qint32> getTypes(MODE mode);
+    QMap<QString,qint32> getStorageClasses(MODE mode);
 };
 
 #endif // XDEMANGLE_H
