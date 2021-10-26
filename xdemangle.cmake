055308f1fe71b17b42c858ef4099f95f96573645
horsicq <horsicq@gmail.com>
2021-10-26 22:52:21 +0200

    Fix: 2021-10-26

--- xdemangle.cmake
+++ xdemangle.cmake
@@ -1,4 +1,6 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../XLLVMDemangler/3rdparty/)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../XLLVMDemangler/3rdparty/llvm/Demangle/)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../XCppfilt/xcppfilt.cmake)
 
