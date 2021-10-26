b7287816f81562be800c2d6f4d983265e0e026ed
horsicq <horsicq@gmail.com>
2021-10-26 18:55:55 +0200

    Fix: 2021-10-26

--- xdemangle.cmake
+++ xdemangle.cmake
@@ -1,5 +1,8 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
+include(${CMAKE_CURRENT_LIST_DIR}/../XCppfilt/xcppfilt.cmake)
+
 set(XDEMANGLE_SOURCES
+    ${XCPPFILT_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xdemangle.cpp
 )
