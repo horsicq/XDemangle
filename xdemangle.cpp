d84cd569ada8e425f6d725cffd94d80af3a12882
horsicq <horsicq@gmail.com>
2022-04-14 01:49:11 +0200

    Fix: 2022-04-14

--- xdemangle.cpp
+++ xdemangle.cpp
@@ -4503,7 +4503,7 @@ QMap<QString, quint32> XDemangle::getPointerTypes(XDemangle::MODE mode)
     return mapResult;
 }
 
-QMap<QString, quint32> XDemangle::getStorageClasses(XDemangle::MODE mode)
+QMap<QString,quint32> XDemangle::getStorageClasses(XDemangle::MODE mode)
 {
     QMap<QString,quint32> mapResult;
 
