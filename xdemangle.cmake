f95de436e22b1814879e0958684beff780903718
horsicq <horsicq@gmail.com>
2024-12-04 08:39:14 +0100

    Update file(s): 2024-12-04

--- xdemangle.cmake
+++ xdemangle.cmake
@@ -2,9 +2,13 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 include_directories(${CMAKE_CURRENT_LIST_DIR}/../XLLVMDemangler/3rdparty/)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/../XLLVMDemangler/3rdparty/llvm/Demangle/)
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XCppfilt/xcppfilt.cmake)
+if (NOT DEFINED XCPPFILT_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XCppfilt/xcppfilt.cmake)
+    set(XDEMANGLE_SOURCES ${XDEMANGLE_SOURCES} ${XCPPFILT_SOURCES})
+endif()
 
 set(XDEMANGLE_SOURCES
+    ${XDEMANGLE_SOURCES}
     ${XCPPFILT_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xdemangle.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xdemangle.h
